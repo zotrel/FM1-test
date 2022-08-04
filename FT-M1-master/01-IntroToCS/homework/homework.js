88c4d2d14015a5b76261dc99c8785bd385713457
Flor March <106163869+zotrel@users.noreply.github.com>
2022-08-04 11:08:54 -0300

    second commit

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,12 +1,33 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  const array = num.toString().split("").reverse().join("");
+// array = array.parseInt();
+//    console.log(array);
+   let array2 = 0;
+   for (let i = 0; i < array.length; i++) {
+        
+        array2 += (array[i]*(Math.pow(2,[i])));    
+       
+
+   }
+   return array2
+
+
 
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+ let dec = [];
+ let resto = 0;
+ while (num > 0){
+     resto = num % 2;
+     num =Math.floor(num/2)
+     dec.unshift(resto);
+ }     
+ let result =  dec.join("");
+ return result;
+
 
 }
 
